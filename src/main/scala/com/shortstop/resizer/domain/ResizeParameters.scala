f1ad34a78079419ced609957007ab023adb17739
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-03-21 18:43:42 +0200

    Fixed resize parameters. Added values for implicit conversion.

--- src/main/scala/com/shortstop/resizer/domain/ResizeParameters.scala
+++ src/main/scala/com/shortstop/resizer/domain/ResizeParameters.scala
@@ -1,5 +1,13 @@
 package com.shortstop.resizer.domain
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import net.liftweb.json.{DateFormat, Formats}
+import spray.http.{HttpEntity, MediaTypes}
+import spray.httpx.unmarshalling.Unmarshaller
+import net.liftweb.json.Serialization._
+
 /**
  * Resize parameters.
  *
@@ -8,7 +16,35 @@ package com.shortstop.resizer.domain
  * @param height  value of height which image should have after resizing
  * @param width   value of width which image should have after resizing
  */
-case class ResizeParameters(key: Option[String] = None,
-                            image: Option[String] = None,
-                            height: Option[Long] = None,
-                            width: Option[Long] = None)
+case class ResizeParameters(key: String,
+                            image: String,
+                            height: Int,
+                            width: Int)
+
+/**
+ * Provides implicit values for conversion of resize parameters from
+ * json to an instance of the ResizeParameters class.
+ */
+object ResizeParameters {
+
+  implicit val liftJsonFormats = new Formats {
+    val dateFormat = new DateFormat {
+      val sdf = new SimpleDateFormat("yyyy-MM-dd")
+
+      def parse(s: String): Option[Date] = try {
+        Some(sdf.parse(s))
+      } catch {
+        case e: Exception => None
+      }
+
+      def format(d: Date): String = sdf.format(d)
+    }
+  }
+
+  implicit val ResizeParametersUnmarshaller =
+    Unmarshaller[ResizeParameters](MediaTypes.`application/json`
+    ) {
+      case HttpEntity.NonEmpty(contentType, data) =>
+        read[ResizeParameters](data.asString)
+    }
+}
