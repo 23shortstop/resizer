6c1c07f37d35d12013cc9eaf3061e0cc9572bfed
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-03-21 18:38:56 +0200

    Added DAO for ResizingResult entities.

---
+++ src/main/scala/com/shortstop/resizer/dao/ResizingResultDAO.scala
@@ -0,0 +1,45 @@
+package com.shortstop.resizer.dao
+
+import java.sql.SQLException
+import com.shortstop.resizer.dao.DataBase._
+import com.shortstop.resizer.domain._
+import scala.slick.driver.MySQLDriver.simple.Database.threadLocalSession
+import scala.slick.driver.MySQLDriver.simple._
+import scala.slick.jdbc.meta.MTable
+
+/**
+ * Provides DAO for ResizingResult entities for MySQL database.
+ */
+class ResizingResultDAO {
+
+  // create a table if not exist
+  db.withSession {
+    if (MTable.getTables("resizing_results").list().isEmpty) {
+      ResizingResults.ddl.create
+    }
+  }
+
+  /**
+   * Creates and saves into db an instance of ResizingResult.
+   *
+   * @param userId an id of user who made request for resizing
+   * @param height value of image height after resizing
+   * @param width  value of image width after resizing
+   * @return
+   */
+  def create(userId: Long, height: Int, width: Int): Either[Failure, ResizingResult] = {
+    try {
+      val resizingResult = ResizingResult(None, userId, "", "", height, width)
+      val id = db.withSession {
+        ResizingResults returning ResizingResults.id insert resizingResult
+      }
+      Right(resizingResult.copy(
+        id = Some(id),
+        original = s"images/$userId/origin/$id",
+        resized = s"images/$userId/resized/$id"))
+    } catch {
+      case e: SQLException =>
+        Left(databaseError(e))
+    }
+  }
+}
